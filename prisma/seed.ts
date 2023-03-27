929a47ed20c6e063a989c95697af7af7c469de66
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-03-27 13:16:14 -0300

    chore: configured prettier formatter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd';
-const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000');
+const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
+const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000')
 
-const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297';
-const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000');
+const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
+const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
 
-const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00';
-const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000');
+const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
+const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000')
 
 async function run() {
-  await prisma.habit.deleteMany();
+  await prisma.habit.deleteMany()
 
-  await prisma.day.deleteMany();
+  await prisma.day.deleteMany()
 
   await Promise.all([
     prisma.habit.create({
@@ -53,7 +53,7 @@ async function run() {
         },
       },
     }),
-  ]);
+  ])
 
   await Promise.all([
     prisma.day.create({
@@ -84,17 +84,17 @@ async function run() {
         },
       },
     }),
-  ]);
+  ])
 }
 
 run()
   .then(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   })
   .catch(async (err) => {
-    console.error(err);
+    console.error(err)
 
-    await prisma.$disconnect();
+    await prisma.$disconnect()
 
-    process.exit(1);
-  });
+    process.exit(1)
+  })
